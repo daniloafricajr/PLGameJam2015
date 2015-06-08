5263f276ed073f0f4a18e4f1dad792aa2ae6e3fe
Danilo Africa, Jr <danilo.a@pocketplaylab.com>
2015-06-08 18:09:09 +0800

    updated with assets

---
+++ Assets/Game/Scripts/Contorls/ScrollCamera.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollCamera : MonoBehaviour {
+
+	public Transform cameraPos;
+	public Transform minPos;
+	public Transform maxPos;
+
+	public bool isScrolling;
+
+	void Update()
+	{
+		isScrolling = false;
+		if (Input.touchCount > 2)
+		{
+			isScrolling = true;
+			if (Input.touches[0].phase == TouchPhase.Moved || Input.touches[1].phase == TouchPhase.Moved)
+			{
+				if (Input.touches[0].phase == TouchPhase.Moved)
+				{
+					Vector3 pos = cameraPos.position;
+					pos += new Vector3(Input.touches[0].deltaPosition.x, Input.touches[0].deltaPosition.y) * Time.deltaTime;
+					pos.x = Mathf.Clamp(pos.x, minPos.position.x, maxPos.position.x);
+					pos.y = Mathf.Clamp(pos.y, minPos.position.y, maxPos.position.y);
+					pos.z = Mathf.Clamp(pos.z, minPos.position.z, maxPos.position.z);
+					cameraPos.position = pos;
+				}
+				else if (Input.touches[1].phase == TouchPhase.Moved)
+				{
+					Vector3 pos = cameraPos.position;
+					pos += new Vector3(Input.touches[1].deltaPosition.x, Input.touches[1].deltaPosition.y) * Time.deltaTime;
+					pos.x = Mathf.Clamp(pos.x, minPos.position.x, maxPos.position.x);
+					pos.y = Mathf.Clamp(pos.y, minPos.position.y, maxPos.position.y);
+					pos.z = Mathf.Clamp(pos.z, minPos.position.z, maxPos.position.z);
+					cameraPos.position = pos;
+				}
+			}
+		}
+	}
+}
